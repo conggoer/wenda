8afa58f7b55e676c7a9a50663ee5022bfb4d51ce
seven <conggoer@163.com>
2018-08-13 13:47:23 +0800

    first

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Description:
+ * Author:
+ * Date:
+ */
+public class Test {
+    public static void main(String[] args){
+        System.out.print("first\n");
+        System.out.print("second");
+    }
+}
